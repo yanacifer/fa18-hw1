d5641c5e76adc146debb02226e1c121c9e208f2c
aivantg <aivant.developer@gmail.com>
2018-09-13 11:57:26 -0700

    initial commit

---
+++ foobar.rb
@@ -0,0 +1,8 @@
+class Foobar
+
+  def self.baz(a)
+    # Class method, not an instance method
+    # Call with `Foobar.baz`
+    # Q4 CODE HERE
+  end
+end
---
+++ hw1.rb
@@ -0,0 +1,52 @@
+def squared_sum(a, b)
+  # Q1 CODE HERE
+
+end
+
+def sort_array_plus_one(a)
+  # Q2 CODE HERE
+
+end
+
+def combine_name(first_name, last_name)
+  # Q3 CODE HERE
+
+end
+
+def blockin_time(a)
+  # DO NOT EDIT THIS CODE BELOW
+  require './foobar'
+  Foobar.baz a
+end
+
+def scrabble(word)
+  values = {
+    a: 1,
+    b: 3,
+    c: 3,
+    d: 2,
+    e: 1,
+    f: 4,
+    g: 2,
+    h: 4,
+    i: 1,
+    j: 8,
+    k: 5,
+    l: 1,
+    m: 3,
+    n: 1,
+    o: 1,
+    p: 3,
+    q: 10,
+    r: 1,
+    s: 1,
+    t: 1,
+    u: 1,
+    v: 4,
+    w: 4,
+    x: 8,
+    y: 4,
+    z: 10,
+  }
+  # Q5 CODE HERE
+end
